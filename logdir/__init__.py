23f5043e1d177bf482e82a066d9743cfd57a97a6
btjanaka <bryon@btjanaka.net>
2020-10-31 02:55:11 -0700

    Add file creation methods

--- logdir/__init__.py
+++ logdir/__init__.py
@@ -5,7 +5,8 @@ __all__ = [
     "LogDir",
 ]
 
-import time
+import datetime
+from collections import namedtuple
 from pathlib import Path
 
 
@@ -15,6 +16,8 @@ class LogDir:
     Creates a logging directory on startup; comes with a handful of other handy
     methods.
 
+    [logdir.LogDir.add_readme][]
+
     Attributes:
         rootdir (pathlib.Path): The root directory for log directories.
         logdir (pathlib.Path): The log directory itself.
@@ -23,11 +26,17 @@ class LogDir:
     def __init__(self, name, rootdir="logs"):
         """Initializes by creating the logging directory.
 
-        The directory is created under rootdir (which is created if it does not
-        exist). Logging directory is named with the date, followed by the time,
-        followed by the given name in lowercase with spaces and underscores
-        replaced with dashes, for instance
-        ``2020-02-14_18:01:45_my-logging-dir``.
+        The directory is created under `rootdir` (which is created if it does
+        not exist). Logging directory is named with the date, followed by the
+        time, followed by the given `name` in lowercase with spaces and
+        underscores replaced with dashes, for instance
+        `2020-02-14_18:01:45_my-logging-dir`.
+
+        Args:
+            name (str or pathlib.Path): Customizable part of the logging
+                directory name, e.g. `My cool experiment`
+            rootdir (str or pathlib.Path): Root directory for all logging
+                directories, e.g. `./logs/`
         """
         # Create the rootdir.
         self.rootdir = Path(rootdir)
@@ -35,7 +44,44 @@ class LogDir:
             self.rootdir.mkdir(parents=True)
 
         # Create the logdir.
-        dirname = (time.strftime("%Y-%m-%d_%H-%M-%S") + "_" +
+        dirname = (datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_" +
                    name.lower().replace("_", "-").replace(" ", "-"))
         self.logdir = self.rootdir / Path(dirname)
         self.logdir.mkdir()
+
+    def file(self, filename):
+        """Returns a string path to the given file.
+
+        Intermediate directories are created if they do not exist.
+
+        Example:
+            Basic usage:
+            ```python
+            logdir = LogDir("logdir")
+            logdir.filepath("file.txt") # "..._logdir/file.txt"
+            ```
+            Creating intermediate directories:
+            ```python
+            logdir = LogDir("logdir")
+            logdir.filepath("newdir/file.txt") # "..._logdir/newdir/file.txt"
+            ```
+        Args:
+            filename (str or pathlib.Path): The name of the file.
+        Returns:
+            (str): Path to the new file in the logging directory.
+        """
+        return str(self.pfile(filename))
+
+    def pfile(self, filename):
+        """Same as [file][logdir.LogDir.file], but returns pathlib.Path.
+
+        Args:
+            filename (str or pathlib.Path): The name of the file.
+        Returns:
+            (pathlib.Path): Path to the new file in the logging directory.
+        """
+        filename = self.logdir / Path(filename)
+        if not filename.parent.exists():
+            filename.parent.mkdir(parents=True)
+        filename.touch()
+        return filename
--- tests/logdir_test.py
+++ tests/logdir_test.py
@@ -1,5 +1,41 @@
 """Tests for logdir."""
+from pathlib import Path
+
+from freezegun import freeze_time
+from logdir import LogDir
+
+# pylint: disable = missing-function-docstring
+
+TIME = "2020-10-31 06:20:45"
+TIME_STR = "2020-10-31_06-20-45"
 
 
-def test_placeholder():
-    pass
+@freeze_time(TIME)
+def test_creates_dir_on_init(tmp_path):
+    LogDir("My Logging Dir", tmp_path)
+    expected_path = tmp_path / f"{TIME_STR}_my-logging-dir"
+    assert expected_path.exists()
+
+
+@freeze_time(TIME)
+def test_creates_nested_dir_on_init(tmp_path):
+    LogDir("My Logging Dir", tmp_path / "logs")
+    expected_path = tmp_path / "logs" / f"{TIME_STR}_my-logging-dir"
+    assert expected_path.exists()
+
+
+@freeze_time(TIME)
+def test_creates_file(tmp_path):
+    logdir = LogDir("My Logging Dir", tmp_path)
+    file = logdir.file("file.txt")
+    assert Path(file) == (tmp_path / f"{TIME_STR}_my-logging-dir" / "file.txt")
+    assert Path(file).exists()
+
+
+@freeze_time(TIME)
+def test_creates_nested_file(tmp_path):
+    logdir = LogDir("My Logging Dir", tmp_path)
+    file = logdir.file("newdir/file.txt")
+    assert Path(file) == (tmp_path / f"{TIME_STR}_my-logging-dir" / "newdir" /
+                          "file.txt")
+    assert Path(file).exists()
