e72411c36bf56230511cbdb27917c8977b90be05
btjanaka <bryon@btjanaka.net>
2020-11-01 11:50:15 -0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 
 setup(
     name="logdir",
-    version="0.2.0",
+    version="0.2.1",
     author="Bryon Tjanaka",
     author_email="bryon@btjanaka.net",
     description="A utility for managing logging directories.",
